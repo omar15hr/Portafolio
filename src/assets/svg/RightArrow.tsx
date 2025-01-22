ef0d087c894bbc07378028bf2ca44ecd500bedcd
omar <omarhrn@gmail.com>
2025-01-22 11:01:46 -0300

    Header listo, continuamos con los proyectos

---
+++ src/assets/svg/RightArrow.tsx
@@ -0,0 +1,5 @@
+export function RightArrow() {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg" width="20"  height="20"  viewBox="0 0 24 24"  fill="#ffffff"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>
+  )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { Github } from "../assets/svg/Github";
+import { Gmail } from "../assets/svg/Gmail";
+import { Linkedin } from "../assets/svg/Linkedin";
+import { RightArrow } from "../assets/svg/RightArrow";
+
 export function Header() {
   return (
-    <div className="header">
+    <div className="header md:flex-row sm:flex-col-reverse">
 
-      <div className="prueba2">
+      <div className="header-description-container">
         <span className="available-card">Disponible</span>
         <div className="header-name md:text-6xl sm:text-4xl">
           Omar Hernández
@@ -10,15 +15,21 @@ export function Header() {
         <div className="career-title md:text-3xl sm:text-xl">
           Desarrollador Frontend
         </div>
-        <p className="header-description">
+        <p className="header-description md:text-xl sm:text-xl">
           Con <b>1 año de experiencia</b> creando soluciones tecnológicas para
           empresas y proyectos personales. Mi meta es crear un{" "}
           <b>impacto positivo</b> en la industria a través de mi trabajo, por
           eso me esfuerzo todos los días en mejorar mi habilidad y
           conocimientos.
         </p>
+        <div className="social-links">
+          <a href=""><Linkedin /></a>
+          <a href=""><Github /></a>
+          <a href=""><Gmail /></a>
+          <a href="" className="cv-button">Curriculum Vitae<RightArrow /></a>
+        </div>
       </div>
-      <div className="prueba">
+      <div className="header-image-container sm:mb-14">
         <img
           src="https://avatars.githubusercontent.com/u/72213827?v=4"
           alt="Omar Hernández"
