6d2a43591ccb66751f8f522b7e8b3708ecc649ef
omar <omarhrn@gmail.com>
2025-01-23 18:03:10 -0300

    Cambios en exp

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,8 +4,8 @@ const experience = [
     company: "Dyatec",
     position: "Desarrollador FullStack",
     workDone: [
-      "Diseñe landing page del sitio web de la OTEC de La Florida. Enfocado a las noticias y cursos que ofrecen. Utilicé CSS y HTML.",
-      "Desarrollé bloque de Moodle, que permite a los administradores del sitio obtener información sobre el status de los alumnos y sus cursos. Utilicé PHP, MySQL, HTML y CSS.",
+      "Diseñe el landing page del sitio web de la OTEC de La Florida. Enfocado a las noticias y cursos que ofrecen. Utilicé CSS y HTML.",
+      "Desarrollé un bloque de Moodle, que permite a los administradores del sitio obtener información sobre el status de los alumnos y sus cursos. Utilicé PHP, MySQL, HTML y CSS.",
       "Modifiqué un plugin de Moodle que permite a los usuarios del sitio descargas masivas de certificados de cursos. Utilicé PHP, MySQL y HTML.",
       "Elaboré una documentación sobre el bloque de Moodle, donde explicaban los pasos necesarios para instalar y configurar el bloque. Utilicé Markdown y HTML.",
     ],
