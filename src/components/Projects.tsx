194bc3dfc56d2bff3648a83f28b38b9686ba7167
omar <omarhrn@gmail.com>
2025-01-23 11:15:14 -0300

    Diseño de card de proyectos. falta responsive

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,19 +3,25 @@ const projects = [
     name: 'Proyecto 1',
     description: 'Descripción del proyecto 1',
     image: 'https://i.ibb.co/4b2r0rX/logo-react.png',
-    url: 'https://github.com'
+    url: 'https://github.com',
+    workFrom: 'Casa',
+    stack: ['React', 'TypeScript', 'TailwindCSS']
   },
   {
     name: 'Proyecto 2',
     description: 'Descripción del proyecto 2',
     image: 'https://i.ibb.co/4b2r0rX/logo-react.png',
-    url: 'https://github.com'
+    url: 'https://github.com',
+    workFrom: 'Casa',
+    stack: ['React', 'TypeScript', 'TailwindCSS']
   },
   {
-    name: 'Proyecto 3',
-    description: 'Descripción del proyecto 3',
+    name: 'Muscari Clothing',
+    description: 'E-commerce de ropa para mujeres, hombres y niños. Diseñé y desarrollé la aplicación en conjunto con mi equipo de trabajo. Contiene funcionalidades de una tienda virtual con pasarela de pago y envío de correos electrónicos. Lo que caracteriza a esta aplicación es su diseño minimalista y su guía de talla según la prenda escogida.',
     image: 'https://i.ibb.co/4b2r0rX/logo-react.png',
-    url: 'https://github.com'
+    url: 'https://github.com',
+    workFrom: 'Proyecto de Titulo Duoc UC',
+    stack: ['React', 'TypeScript', 'Next Js', 'TailwindCSS', 'PostgreSQL', 'Github', 'Git']
   }
 ]
 
@@ -31,7 +37,19 @@ export function Projects() {
               <div className="project-card" key={project.name}>
                 <img src={project.image} alt={project.name} className="project-img" />
                 <div className="card-content">
-                  <h3 className="card-title md:text-3xl sm:text-2xl">{project.name}</h3>
+                  <h3 className="card-title md:text-3xl sm:text-2xl">
+                    {project.name}
+                    <span className="card-span"> - {project.workFrom}</span>
+                  </h3>
+                  <div className="card-stack">
+                    {
+                      project.stack.map(stack => (
+                        <div className="stack-item-project" key={stack}>
+                          {stack}
+                        </div>
+                      ))
+                    }
+                  </div>
                   <p className="card-description">{project.description}</p>
                   <a href={project.url} className="card-link">Ver más</a>
                 </div>
