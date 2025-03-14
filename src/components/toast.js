0312a566d83c32f0d1e1881bd437d72864096b51
omar <omarhrn@gmail.com>
2025-03-13 23:00:35 -0300

    Portafolio listo primera parte2

--- src/components/toast.js
+++
@@ -1,7 +0,0 @@
-import { toast } from 'sonner';
-
-export function copyToClipboard(text) {
-  navigator.clipboard.writeText(text).then(() => {
-    toast.success("Correo copiado al portapeles");
-  });
-}
